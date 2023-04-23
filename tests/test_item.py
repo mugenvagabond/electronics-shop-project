99b6fd712b45a7d356bf5db80ad81afab1ccd79e
Nikita Vlakhno <monkruus96@gmail.com>
2023-04-23 21:11:08 +0300

    Добавлены тесты на методы класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item3():
+    return Item("ПК", 100000, 1)
+
+
+def test_calculate_total_price(item3):
+    assert item3.calculate_total_price() == 100000
+
+
+def test_apply_discount(item3):
+    Item.pay_rate = 0.8
+    item3.apply_discount()
+    assert item3.price == 80000.0
