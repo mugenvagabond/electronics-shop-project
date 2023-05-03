adb8018c8e358ca6b9d0b01d8f5df55a2d42c2a4
Nikita Vlakhno <monkruus96@gmail.com>
2023-05-04 00:59:14 +0300

    Внесены следующие изменения в класс Item:
    - атрибут name - приватный
    - добавлен геттер и сеттер для name, используя @property
    - в сеттере name проверяется, что длина наименования товара не больше 10 симвовов
    - instantiate_from_csv() - добавлен класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
    - string_to_number() - добавлен статический метод, возвращающий число из числа-строки

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,51 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+        # self.all.append(self)
+
+    @property
+    def name(self):
+        """
+        Декоратор, делающий поле name private
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Декоратор, позволяющий атрибуту name в режиме private присваивать новые значения при обращении через
+        экземпляр класса к атрибуту.
+        Выполняет проверку на количество символов в строке и возвращает соответствующие сообщения
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception("Длина наименования товара превышает 10 символов")
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        Возвращает строку с данными о количестве записей о товарах
+        """
+        path = r"C:\Users\Cyber\PycharmProjects\electronics-shop-project\src\items.csv"
+
+        with open(path, newline='') as file:
+            reader = csv.DictReader(file)
+            cls.all.clear()
+            for row in reader:
+                cls.all.append(cls(row['name'], float(row['price']), int(row['quantity'])))
+        return len(cls.all)
+
+    @staticmethod
+    def string_to_number(number):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        return int(float(number))
 
     def calculate_total_price(self) -> float:
         """
