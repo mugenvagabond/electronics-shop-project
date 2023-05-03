8321b1b94f5dab0432c41de0471dee6d5f3b0f44
Nikita Vlakhno <monkruus96@gmail.com>
2023-05-04 01:25:38 +0300

    Добавлены тесты для методов __repr__ и __str__

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,6 +15,14 @@ def item1():
     return Item("Смартфон", 10000, 20)
 
 
+def test_repr(item1):
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str(item1):
+    assert str(item1) == "Смартфон"
+
+
 def test_calculate_total_price(item1):
     assert item1.calculate_total_price() == 200000
 
