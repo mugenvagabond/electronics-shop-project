ea918e57eac2f8ffc080ece27853074b41347bb2
Nikita Vlakhno <monkruus96@gmail.com>
2023-05-04 00:59:28 +0300

    Добавлены тесты для новых методов

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,16 +2,37 @@ import pytest
 from src.item import Item
 
 
+def test_name():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    with pytest.raises(Exception, match='Длина наименования товара превышает 10 символов'):
+        item.name = 'Телекоммуникатор'
+
+
 @pytest.fixture
-def item3():
-    return Item("ПК", 100000, 1)
+def item1():
+    return Item("Смартфон", 10000, 20)
 
 
-def test_calculate_total_price(item3):
-    assert item3.calculate_total_price() == 100000
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 200000
 
 
-def test_apply_discount(item3):
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_apply_discount(item1):
     Item.pay_rate = 0.8
-    item3.apply_discount()
-    assert item3.price == 80000.0
+    item1.apply_discount()
+    assert item1.price == 8000.0
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
