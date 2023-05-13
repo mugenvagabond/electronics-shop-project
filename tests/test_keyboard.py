4afa331a4a10b961e36312c64304fc7fb1e0ad8c
Nikita Vlakhno <monkruus96@gmail.com>
2023-05-14 02:11:14 +0300

    Добавлены тесты методов класса KeyBoard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,31 @@
+from src.keyboard import KeyBoard
+from src.item import Item
+import pytest
+
+
+kb = KeyBoard('Dark Project KD87A', 9600, 5)
+
+
+@pytest.fixture
+def kb1():
+    return KeyBoard('Dark Project KD87A', 9600, 5)
+
+
+def test_str(kb1):
+    assert str(kb1) == 'Dark Project KD87A'
+
+
+def test_str_language(kb1):
+    assert str(kb1.language) == "EN"
+
+
+def test_change_lang(kb1):
+    kb1.change_lang()
+    assert str(kb1.language) == "RU"
+    kb1.change_lang().change_lang()
+    assert str(kb1.language) == "RU"
+
+
+def test_private_language(kb1):
+    with pytest.raises(AttributeError, match="property 'language' of 'KeyBoard' object has no setter"):
+        kb1.language = 'CH'
